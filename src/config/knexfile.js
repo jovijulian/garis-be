36fd96202b80674bb2330beb66ff3f4b3eab603a
jovijulian <jovijulian2002@gmail.com>
2025-09-01 16:25:16 +0700

    fix: set up railway

--- src/config/knexfile.js
+++ src/config/knexfile.js
@@ -3,14 +3,13 @@ require('dotenv').config();
 module.exports = {
   development: {
     client: 'mysql2',
-    connection: process.env.DATABASE_URL, // <-- CUKUP SATU BARIS INI
+    connection: process.env.DATABASE_URL, 
     pool: { min: 2, max: 10 }
   },
 
-  // SANGAT DISARANKAN: Tambahkan juga environment untuk production
   production: {
     client: 'mysql2',
-    connection: process.env.DATABASE_URL, // <-- Variabel yang sama
+    connection: process.env.DATABASE_URL,
     pool: { min: 2, max: 10 }
   }
 };
